615c9fa6ce44d8a5e6daf8880493d009020f2598
kinjatoro <136925952+kinjatoro@users.noreply.github.com>
2024-11-22 15:42:22 -0300

    cambios tablas

--- create/crear-tablas-raw.sql
+++ create/crear-tablas-raw.sql
@@ -1,5 +1,5 @@
 CREATE TABLE raw_usuarios (
-    id_usuario INT PRIMARY KEY,
+    id_usuario NVARCHAR(100) PRIMARY KEY,
     nombre NVARCHAR(MAX),
     tipo_usuario NVARCHAR(MAX), -- Puede ser 'locador', 'locatario', 'empleado', 'mudanza', 'abogado'
     fecha_registro DATE,
@@ -15,7 +15,7 @@ CREATE TABLE raw_publicaciones (
     latitud DECIMAL(10,6),
     longitud DECIMAL(10,6),
     estado NVARCHAR(10), --- Activo o inactivo
-    id_usuario INT,
+    id_usuario NVARCHAR(100),
     tipo NVARCHAR(15), --- Casa o departamento
     superficie_total_m2 INT, --- En metros cuadrados,
     ganancia_generada DECIMAL(15,2),
@@ -27,7 +27,7 @@ CREATE TABLE raw_pagos (
     fecha DATE,
     monto DECIMAL(15,2),
     id_publicacion INT, 
-    id_usuario INT,
+    id_usuario NVARCHAR(100),
     estado NVARCHAR(MAX), --- 'Aprobado, pendiente, rechazado'
     FOREIGN KEY (id_publicacion) REFERENCES raw_publicaciones(id_publicacion),
     FOREIGN KEY (id_usuario) REFERENCES raw_usuarios(id_usuario)
@@ -44,7 +44,7 @@ CREATE TABLE raw_mudanzas (
     longitud_origen DECIMAL(10,6),
     latitud_destino DECIMAL(10,6),
     longitud_destino DECIMAL(10,6),
-    id_usuario INT,
+    id_usuario NVARCHAR(100),
     FOREIGN KEY (id_usuario) REFERENCES raw_usuarios(id_usuario)
 );
 
@@ -54,7 +54,7 @@ CREATE TABLE raw_financiamientos (
     monto_solicitado DECIMAL(15,2),
     monto_aprobado DECIMAL(15,2),
     estado_solicitud NVARCHAR(MAX), ---- ['aprobado', 'pendiente', 'rechazado']
-    id_usuario INT,
+    id_usuario NVARCHAR(100),
     FOREIGN KEY (id_usuario) REFERENCES raw_usuarios(id_usuario)
 );
 
@@ -71,9 +71,9 @@ Rechazado: cuando alguna de las partes o el escribano rechazan el contrato antes
 CREATE TABLE raw_contratos (
     id_contrato INT PRIMARY KEY,
     id_publicacion INT, 
-    id_usuario_locatario INT,
-    id_usuario_locador_o_mudanza INT,
-    id_usuario_escribano INT, -- Nuevo campo para el escribano (firma digital)
+    id_usuario_locatario NVARCHAR(100),
+    id_usuario_locador_o_mudanza NVARCHAR(100),
+    id_usuario_escribano NVARCHAR(100), -- Nuevo campo para el escribano (firma digital)
     tipo_contrato NVARCHAR(50), -- ['Alquiler', 'Guardado muebles', 'Mudanza']
     fecha_firma DATE,
     fecha_inicio DATE,
@@ -89,9 +89,9 @@ CREATE TABLE raw_contratos (
 
 CREATE TABLE raw_reclamos (
     id_reclamo INT PRIMARY KEY,
-	fecha_reclamo DATE,
+    fecha_reclamo DATE,
     estado NVARCHAR(50), --  ("Abierto", "En Proceso", "Resuelto", "Cerrado")
-	id_usuario INT,
-	categoria NVARCHAR(50), --  (Categoría específica como "Problemas Técnicos", "Cobros Incorrectos", "Servicio Deficiente", “Otros”, etc.)
+    id_usuario NVARCHAR(100),
+    categoria NVARCHAR(50), --  (Categoría específica como "Problemas Técnicos", "Cobros Incorrectos", "Servicio Deficiente", “Otros”, etc.)
     FOREIGN KEY (id_usuario) REFERENCES raw_usuarios(id_usuario),  
 );
